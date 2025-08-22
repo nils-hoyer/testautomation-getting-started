967dfd769c665ff7b5055c706a52774eff4c9f10
codesurfer.io <hoyer.nils@gmail.com>
2025-08-22 14:58:22 +0200

    create course setup

---
+++ tests/login.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('should login successfully', async ({ page }) => {
+  //GIVEN
+  await page.goto('https://test-boutique.vercel.app/');
+  
+  //WHEN
+  await page.getByTestId('login-icon').click();
+  await page.getByTestId('login-email').fill('brad@pitt.de');
+  await page.getByTestId('login-password').fill('1234563');
+  await page.getByTestId('login-button').click();
+
+  //THEN
+  await expect(page.getByTestId('user-avatar')).toContainText('BP');
+});
